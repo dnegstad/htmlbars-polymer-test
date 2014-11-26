c3f35dc28b693215eece0233cd4cad0d1e3d2215
David Negstad <dnegstad@gmail.com>
2014-11-25 17:44:07 -0800

    Broken dialog box example

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -1,7 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  showDialog: false,
+
   actions: {
+    popup: function() {
+      this.set('showDialog', true);
+    },
+
+    opened: function() {
+      console.log('opened?');
+    },
+
     switch: function() {
       var selected = this.get('model.selected');
 
